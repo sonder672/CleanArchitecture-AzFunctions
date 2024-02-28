2a883401632b9f32dfbe8caf346d3393127f7ae7
sLargo <slargodiaz@gmail.com>
2024-02-27 21:49:28 -0500

    Capa IoC y AzureFunctions TriggerHttp
    Se crea capa IoC que sirve para resolver dependencias de capas. Se
    genera proyecto AzureFunctions TriggerHttp y se consume el puerto de
    entrada de la capa de servicios y se obtiene la respuesta del puerto de
    salida
    
    Signed-off-by: sLargo <slargodiaz@gmail.com>

---
+++ TaskMicroservice/TaskMicroservice.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskMicroservice.EFCore;
+using TaskMicroservice.Presenters;
+using TaskMicroservice.Services;
+
+namespace TaskMicroservice.IoC;
+
+public static class DependencyContainer
+{
+    public static IServiceCollection AddGeneralDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        // services.AddRepositories(configuration);
+        services.AddUseCasesServices();
+        services.AddPresenters();
+
+        return services;
+    }
+}
